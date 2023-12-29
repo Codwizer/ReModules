eaea711daf53a7e5c2badfde85c15afd70cec414
CodWiz <sergeimatrakhib@yandex.ru>
2023-12-29 22:04:39 +0300

    Add files via upload
    New module on the api @vsecoder
    Thanks @vsecoder

---
+++ face.py
@@ -0,0 +1,40 @@
+# ---------------------------------------------------------------------------------
+# Name: face
+# Description: Random face
+# Author: @hikka_mods
+# ---------------------------------------------------------------------------------
+
+# 🔒    Licensed under the GNU AGPLv3
+# 🌐 https://www.gnu.org/licenses/agpl-3.0.html
+
+# meta developer: @hikka_mods
+# scope: Api face
+# scope: Api face 0.0.1
+# requires: requests
+# ---------------------------------------------------------------------------------
+from hikkatl.types import Message
+import requests
+
+from .. import loader, utils
+
+
+@loader.tds
+class face(loader.Module):
+    """random face"""
+
+    strings = {
+        "name": "face",
+        "loading": (
+            "<emoji document_id=5348399448017871250>🔍</emoji> Ищю вам ascii face"
+        ),
+        "random_face": (
+            "<emoji document_id=5208878706717636743>🗿</emoji> Вот ваш рандомный ascii face\n<code>{}</code>"
+        )
+    }
+
+    async def rfacecmd(self, message: Message):
+        """random ascii face"""
+        await utils.answer(message, self.strings("loading"))
+        response = requests.get("https://vsecoder.dev/api/faces")
+        random_face = response.json()["data"]
+        await utils.answer(message, self.strings("random_face").format(random_face))
